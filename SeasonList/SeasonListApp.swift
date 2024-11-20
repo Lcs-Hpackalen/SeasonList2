264f640e3dd7968aaf745b4a01285db05c255dc4
Hannu Packalen <hpackalen@lcs.on.ca>
2024-11-20 10:14:54 -0500

    Initial Commit

---
+++ SeasonList/SeasonListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeasonListApp.swift
+//  SeasonList
+//
+//  Created by Hannu Packalen on 2024-11-20.
+//
+
+import SwiftUI
+
+@main
+struct SeasonListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
