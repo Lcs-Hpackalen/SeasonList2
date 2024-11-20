94209da4c1ae740003ae72a7fe4f3ec79e4f4951
Hannu Packalen <hpackalen@lcs.on.ca>
2024-11-20 11:00:01 -0500

    Summer season view complete.

--- SeasonList/City.swift
+++ SeasonList/City.swift
@@ -12,7 +12,7 @@ struct Season{
     let seasonDescription: String
     let seasonMonths: String
 }
-let summer =  (seasonName: "summer", seasonDescription: "It is an warm time of month, kids go play otuside and school is on break. Vaction is coming up!", seasonMonths:"June, July, and August")
+let summer =  (seasonName: "summer", seasonDescription: "Summer is an warm time of year, kids go play otuside and school is on break. Vaction is coming up!", seasonMonths:"June, July, and August")
 let winter =  (seasonName: "winter", seasonDescription: "It is a cold time of month, kids go play outside in the snow. Inside celebrating the holidays, on a christmas break!", seasonMonths: "December, January, and Febuary")
 let spring = (seasonName: "Spring'", seasonDescription: "It is an beinging of warm times to come, and flowers to grow. Kids go play in the mud and the raining exciement is to grow!", seasonMonths: "March, April, and May")
 let fall = (seasonName: "Fall", seasonDescription: "It is a time of year when the leaves change color and the trees start to fall. Kids go play in the leaves and the fall is to come!", seasonMonths: "September, October, and November")
--- SeasonList/SwiftUIView.swift
+++ SeasonList/SwiftUIView.swift
@@ -7,16 +7,27 @@
 
 import SwiftUI
 
-struct seasonView: View {
+struct SummerView: View {
     //Mark: stored properties
     
     var body: some View {
-        Image(systemName: "sun.max.fill")
+        VStack{
+            HStack{
+                Image(systemName: "sun.max.fill")
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                Text("Season: \(summer.seasonName)")
+                    .font(.title)
+            }
+            Text("----------------------------------------------------")
+            Text("Time of year: \(summer.seasonMonths)")
+            Text("\(summer.seasonDescription)")
+        }
     }
 }
 
 #Preview {
-    seasonView()
+    SummerView()
 }
 
 
